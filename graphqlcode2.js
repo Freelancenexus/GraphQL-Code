80e38ee1ec0eee938a0c734097393684a21f9d97
Freelancenexus <79074146+Freelancenexus@users.noreply.github.com>
2021-02-18 10:54:15 +0530

    Update graphqlcode2.js

--- graphqlcode2.js
+++ graphqlcode2.js
@@ -1,15 +1,18 @@
 <script>
 	const table = document.getElementById("table");
  	const row = document.getElementById("row");
+  const noRecords = document.getElementById("no-records");
   const loading = document.getElementById("loading");
   const next = document.getElementById("next");
   const previous = document.getElementById("previous");
   const _number = document.getElementById("page-number");
   const _range = document.getElementById("page-range");
+  const modal= document.getElementById("revisionmodal");
 	
   loading.style.display = "block";
   row.style.display = "none";
-  table.style.display = "block";
+  table.style.display = "none";
+  noRecords.style.display = "none";
   previous.style.pointerEvents = "none";
 	previous.style.opacity = 0.5;
 
@@ -38,11 +41,16 @@
       const allDetails = result.data.items_by_column_values.map(item => { return { ...item.column_values.filter(j => headers.includes(j.id)).map (i => { return {...i, value: JSON.parse(i.value)}})} });
       const allIds = result.data.items_by_column_values.map(item => item.id);
       
-      console.log(allDetails)
+      const projectStatus = {
+        0: "In Progress",
+        1: "Finished",
+        2: "In Revision",
+        3: "Revised"
+			}
+      
       currentRows = allDetails.length;
       let i;
       for (i = 0; i < allDetails.length; i++) {
-        console.log(allDetails[i]);
         const details = allDetails[i];
         const id = allIds[i];
         const cln = row.cloneNode(true);
@@ -56,54 +64,61 @@
         } else {
         	columns[8].children[1].style.display = "none";
           columns[8].children[0].style.display = "block";
-        	columns[8].children[0].addEventListener("click", () => {
-        		console.log('row details ---->>>', details, id);            
-            document.getElementById("item-id").innerText = id;
-           	document.getElementById("project-id").innerText = details[0].value;
-            document.getElementById("project-name").innerText = details[1].value;
+        	columns[8].children[0].addEventListener("click", () => {         
+            modal.style.display = "flex";
+           	modal.style.opacity = "1";
             document.getElementById('CognitoRevisionForm').src = document.getElementById('CognitoRevisionForm').src
-            
             var revisionFrame = document.getElementById("CognitoRevisionForm")
-Cognito.prefill(
-	{
-	"ItemID" :"{{wf {&quot;path&quot;:&quot;item-id&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
-  "ItemName" :"{{wf {&quot;path&quot;:&quot;name&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
-  "Email" :"{{wf {&quot;path&quot;:&quot;guild-email&quot;,&quot;type&quot;:&quot;Email&quot;\} }}",
-  "AirtableID" : "{{wf {&quot;path&quot;:&quot;aittable-id&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
-  "AccountType":"{{wf {&quot;path&quot;:&quot;account-type&quot;,&quot;type&quot;:&quot;Number&quot;\} }}",
-  "MondaycomItemID": document.getElementById("item-id").innerText,
-  "ProjectID": "4326",
-  "ProjectName": "eeeeeeeeeeeeeeeee"
-  }, 
-  revisionFrame); 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            Cognito.prefill(
+              {
+              "ItemID" :"{{wf {&quot;path&quot;:&quot;item-id&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
+              "ItemName" :"{{wf {&quot;path&quot;:&quot;name&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
+              "Email" :"{{wf {&quot;path&quot;:&quot;guild-email&quot;,&quot;type&quot;:&quot;Email&quot;\} }}",
+              "AirtableID" : "{{wf {&quot;path&quot;:&quot;aittable-id&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}", 
+              "AccountType":"{{wf {&quot;path&quot;:&quot;account-type&quot;,&quot;type&quot;:&quot;Number&quot;\} }}",
+              "MondaycomItemID": id,
+              "ProjectID": details[0].value ? details[0].value : '',
+              "ProjectName": details[1].value ? details[1].value : ''
+              }, 
+            revisionFrame);  
         	});
         }
-       
-        console.log('columns', columns);
+        
+        let documentTypes = '';
+       	let j;
+        const allDocumentTypes = details[5].value ? details[5].value.text.split(',') : [];
+        for(j = 0; j < allDocumentTypes.length; j++) {
+        	documentTypes = documentTypes + `<li>${allDocumentTypes[j]}</li>`
+        }
+        
+        if (details[7].value === 'Working On It') {
+        	columns[7].children[0].innerText = `Working on it`;
+          columns[7].children[1].style.display = "none";
+        } else {
+          columns[7].children[0].style.display = "none";
+          columns[7].children[1].href = `${details[7].value}`;
+        }
+        
         columns[0].children[0].innerText = `${details[0].value ? details[0].value : ''}`;
         columns[1].children[0].innerText = `${details[1].value ? details[1].value : ''}`;
-        columns[2].children[0].innerText = `${details[2].value ? details[2].value.index : ''}`;
-        columns[3].children[0].innerText = `${details[3].value ? details[3].value : ''}`;
-        columns[4].children[0].innerText = `${details[4].value ? details[4].value.date : ''}`;
-        columns[5].children[0].innerText = `${details[5].value ? details[5].value.date : ''}`;
-        columns[6].children[0].innerText = `${details[6].value ? details[6].value.text : ''}`;
-        columns[7].children[0].innerText = `${details[7].value ? details[7].value : ''}`;
+        columns[2].children[0].innerHTML = documentTypes;
+        columns[3].children[0].innerText = `${details[2].value ? projectStatus[details[2].value.index] : ''}`;
+        columns[4].children[0].innerText = `${details[3].value ? details[3].value.date : ''}`;
+        columns[5].children[0].innerText = `${details[4].value ? details[4].value.date : ''}`;
+        columns[6].children[0].innerText = `${details[6].value ? details[6].value : ''}`;
         table.appendChild(cln); 
       }
       
-      loading.style.display = "none";
-      table.style.display = "block";
-          
+      if (currentRows === 0) {
+        loading.style.display = "none";
+        noRecords.style.display = "block";
+        table.style.display = "none";
+      } else {
+        loading.style.display = "none";
+        noRecords.style.display = "none";
+      	table.style.display = "block";
+      }
+      
       _number.innerText = `Page ${page}`;
       _range.innerText = `${(page - 1) * pageSize + currentRows}/${total} Records`;
     })
@@ -143,14 +158,18 @@ Cognito.prefill(
       const allDetails = result.data.items_by_column_values;
       total = allDetails.length;
       _range.innerText = `${(page - 1) * pageSize + currentRows}/${total} Records`;
-      console.log('total', total)
+     	if (total <= pageSize) {
+      	next.style.pointerEvents = "none";
+				next.style.opacity = 0.5;
+      } else {
+        next.style.pointerEvents = "all";
+				next.style.opacity = 1;
+      }
     })
   }
   
   const setAsAccepted = async (id, column) => {
     const url = 'https://api.monday.com/v2/';
-    
-    console.log(id, column);
     
     await fetch(url, {
       method: 'POST',
@@ -257,12 +276,4 @@ Cognito.prefill(
     }`)
     removeRows(currentRows);
   });
-
-	document.getElementById("test").addEventListener("click", function(){
- 		console.log('clicked');
-  });
-  
-  
-
 </script>
-
